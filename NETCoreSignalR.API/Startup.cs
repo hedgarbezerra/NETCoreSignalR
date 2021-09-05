749e916b013b8d1df3f0580e3cb7055d93c7d116
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2021-09-05 02:07:10 -0300

    Adicionado versionamento da API e classes para configuração do Swagger criadas;

--- NETCoreSignalR.API/Startup.cs
+++ NETCoreSignalR.API/Startup.cs
@@ -1,14 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using NETCoreSignalR.Util.Configuration;
+using Newtonsoft.Json;
+using Serilog;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace NETCoreSignalR.API
@@ -26,19 +43,147 @@ namespace NETCoreSignalR.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHttpContextAccessor();
+
+            services.AddDirectoryBrowser();
+
+            #region Setting json handler
+            services.AddControllers()
+                .AddJsonOptions(ops =>
+                {
+                    ops.JsonSerializerOptions.IgnoreReadOnlyProperties = false;
+                    ops.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    ops.JsonSerializerOptions.IgnoreNullValues = false;
+                    ops.JsonSerializerOptions.WriteIndented = true;
+                    ops.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    ops.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                    ops.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    ops.JsonSerializerOptions.MaxDepth = 64;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            #endregion
+
+            #region API Versioning
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = ApiVersionReader.Combine(new HeaderApiVersionReader("x-api-version"), new QueryStringApiVersionReader("api-version"));
+            });
+            #endregion
+
+            #region Swagger documentation setup
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<SwaggerConfiguration>();
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+            });
+            #endregion
+
+            #region JWT token bearer and authentication setup
+
+            //var appSettings = new APISettings(Configuration);
+            //byte[] tokenKeyBytes = Encoding.ASCII.GetBytes(appSettings.JWTKey);
+
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(x =>
+            //{
+            //    x.RequireHttpsMetadata = false;
+            //    x.SaveToken = true;
+            //    x.IncludeErrorDetails = true;
+            //    x.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(tokenKeyBytes),
+            //        ValidateIssuer = true,
+            //        ValidIssuer = "NET Core API",
+            //        ValidateAudience = false,
+            //        ValidateTokenReplay = true,
+            //        ValidateLifetime = true,
+            //        ClockSkew = TimeSpan.Zero
+            //    };
+            //});
+
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("WebApp",
+            //        builder => builder.WithOrigins("http://example.com").AllowAnyHeader());
+            //});
+            #endregion
+
+            ServicesBinding servicesBinder = new ServicesBinding(Configuration);
+            servicesBinder.BindServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            IApiVersionDescriptionProvider apiVersioningProvider)
         {
+            #region Setting up exception handling
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(c => c.Run(async context =>
+                {
+                    var exception = context.Features
+                        .Get<IExceptionHandlerPathFeature>()
+                        .Error;
+                    var result = JsonConvert.SerializeObject(new { message = exception.Message, stacktrace = exception.StackTrace });
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsync(result);
+                }));
+            }
+            #endregion
+
+            #region setting up logging 
+            var apiConfig = new APISettings(Configuration);
+
+            Log.Logger = new LoggerConfiguration()
+                      .Enrich.FromLogContext()
+                      .WriteTo.MSSqlServer(apiConfig.ConnectionString,
+                          autoCreateSqlTable: true,
+                          restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
+                          tableName: Configuration["Logging:Table"])
+                      .CreateLogger();
+
+
+            #endregion
+
+            #region Setting up swagger for versioning
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                foreach (var description in apiVersioningProvider.ApiVersionDescriptions)
+                {
+                    c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+            #endregion
+
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
---
+++ NETCoreSignalR.API/SwaggerConfiguration.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace NETCoreSignalR.API
+{
+    public class SwaggerConfiguration : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            operation.Deprecated |= apiDescription.IsDeprecated();
+
+            foreach (var responseType in context.ApiDescription.SupportedResponseTypes)
+            {
+                var responseKey = responseType.IsDefaultResponse ? "default" : responseType.StatusCode.ToString();
+                var response = operation.Responses[responseKey];
+
+                foreach (var contentType in response.Content.Keys)
+                {
+                    if (!responseType.ApiResponseFormats.Any(x => x.MediaType == contentType))
+                    {
+                        response.Content.Remove(contentType);
+                    }
+                }
+            }
+
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (parameter.Schema.Default == null && description.DefaultValue != null)
+                {
+                    var json = JsonSerializer.Serialize(description.DefaultValue, description.ModelMetadata.ModelType);
+                    parameter.Schema.Default = OpenApiAnyFactory.CreateFromJson(json);
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+        }
+
+    }
+
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "TalkingBook",
+                Version = description.ApiVersion.ToString(),
+                Description = "TalkingBook API allowing chat between users and security with JWT",
+                Contact = new OpenApiContact
+                {
+                    Name = "Hedgar Bezerra",
+                    Url = new Uri("https://github.com/hedgarbezerra"),
+                    Email = "hedgarbezerra35@gmail.com"
+                }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
